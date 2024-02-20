eae423c441c4ac221558f07de1c527563401ce9b
Constroad <administracion@constroad.com>
2024-02-20 17:02:27 -0500

    feat: excluding .next/cache/webpack - nextconfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        '.next/cache/webpack',
+      ]
+    }
+  }
+}
 
 module.exports = nextConfig

32b2f80d32bc897030c5e04b2656a4f8f2b78964
Constroad <administracion@constroad.com>
2025-06-01 12:18:25 -0500

    fixing vercel errors

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -1,59 +1,58 @@
-// /pages/api/sendMail.ts
+import { NextRequest } from 'next/server'
+import nodemailer from 'nodemailer'
 
-import { NextApiRequest, NextApiResponse } from 'next';
-import nodemailer from 'nodemailer';
+export async function POST(req: NextRequest) {
+  const body = await req.json()
+  const { email, name, message, phone, companyName, ruc, startCity, endCity } = body
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const { email, name, message, phone, companyName, ruc, startCity, endCity } = req.body;
+  const phoneNumber = phone === '' ? '- - -' : phone
+  const Ruc = ruc === '' ? '- - -' : ruc
+  const company = companyName === '' ? '- - -' : companyName
+  const firstCity = startCity === '' ? '- - -' : startCity
+  const secondCity = endCity === '' ? '- - -' : endCity
 
-    const phoneNumber = phone === '' ? '- - -' : phone
-    // const cubos = nroCubos === '' ? '1' : nroCubos
-    const Ruc = ruc === '' ? '- - -' : ruc
-    const company = companyName === '' ? '- - -' : companyName
+  const msg = `
+    <html>
+      <body>
+        <strong style="font-size: 20px;">Estamos procesando tu cotización!</strong><br><br>
+        <strong>Nombre:</strong> ${name}<br>
+        <strong>Razón social:</strong> ${company}<br>
+        <strong>RUC:</strong> ${Ruc}<br><br>
+        <strong>Mensaje:</strong> ${message}<br><br>
+        <strong>Correo electrónico:</strong> ${email}<br>
+        <strong>Teléfono:</strong> ${phoneNumber}<br>
+        <strong>Ciudad de partida:</strong> ${firstCity}<br>
+        <strong>Ciudad de destino:</strong> ${secondCity}<br>
+      </body>
+    </html>
+  `
 
-    const msg = `
-<html>
-  <body>
-    <strong style="font-size: 20px;">Estamos procesando tu cotización!</strong><br><br>
-    <strong>Nombre:</strong> ${name}<br>
-    <strong>Razón social:</strong> ${company}<br>
-    <strong>RUC:</strong> ${Ruc}<br><br>
-    <strong>Mensaje:</strong> ${message}<br><br>
-    <strong>Correo electrónico:</strong> ${email}<br>
-    <strong>Teléfono:</strong> ${phoneNumber}<br>
-    <strong>Ciudad de partida:</strong> ${startCity}<br>
-    <strong>Ciudad de destino:</strong> ${endCity}<br>
-  </body>
-</html>
-`;
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.APP_PASSWORD,
+      },
+    })
 
-    try {
-      const transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-          user: process.env.EMAIL,
-          pass: process.env.APP_PASSWORD,
-        },
-      });
-
-      const mailOptions = {
-        from: process.env.EMAIL,
-        cc: process.env.EMAIL,
-        to: email,
-        subject: `Solicitud de cotizacion para ${companyName}`,
-        html: msg,
-      };
-
-      // Envía el correo electrónico
-      await transporter.sendMail(mailOptions);
-
-      res.status(200).json({ message: 'Correo enviado con éxito' });
-    } catch (error) {
-      console.error('Error al enviar el correo', error);
-      res.status(500).json({ message: 'Error al enviar el correo' });
+    const mailOptions = {
+      from: process.env.EMAIL,
+      cc: process.env.EMAIL,
+      to: email,
+      subject: `Solicitud de cotizacion para ${companyName}`,
+      html: msg,
     }
-  } else {
-    res.status(405).json({ message: 'Método no permitido' });
+
+    await transporter.sendMail(mailOptions)
+
+    return new Response(JSON.stringify({ message: 'Correo enviado con éxito' }), {
+      status: 200,
+    })
+  } catch (error) {
+    console.error('Error al enviar el correo', error)
+    return new Response(JSON.stringify({ message: 'Error al enviar el correo' }), {
+      status: 500,
+    })
   }
 }

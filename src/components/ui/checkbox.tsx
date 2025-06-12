5f0fd36ac84d570dfe5019ea9ebf489f7ef81d94
Constroad <administracion@constroad.com>
2025-06-12 00:52:16 -0500

    fixing error

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 export interface CheckboxProps extends ChakraCheckbox.RootProps {
   icon?: React.ReactNode
   inputProps?: React.InputHTMLAttributes<HTMLInputElement>
-  rootRef?: React.RefObject<HTMLLabelElement | null>
+  rootRef?: React.LegacyRef<HTMLLabelElement> | undefined ; 
 }
 
 export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
